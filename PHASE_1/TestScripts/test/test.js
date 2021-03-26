e4514b98b1d6bebf4856dafe534c9cfda944280f
Max Owen <z5263663@cse.unsw.edu.au>
2021-03-26 16:26:46 +1100

    finish disease testing

--- PHASE_1/TestScripts/test/test.js
+++ PHASE_1/TestScripts/test/test.js
@@ -10,21 +10,26 @@ const express = require('express');
 
 const app = express();
 
-var id = '';
-
 describe('GET /diseases', function(){
-    it('200 response - has salmonella', function(done) {
+    it('200 response and has salmonella', function(done) {
         request('https://thicc-peas-cdc-api-o54gbxra3a-an.a.run.app')
             .get('/diseases')
+            .expect(200)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200)
             .expect(function(res) {
                 var contains = false;
                 for (var i = 0; i < res.body.length; i++) {
                     if (res.body[i].name == 'salmonella') {
-                        id = res.body[i].disease_id;
                         contains = true;
+                        assert(res.body[i].name == 'salmonella');
+                        assert(res.body[i].disease_id == '777d1c109f5eef1d64c418062a918d33');
+                        var hardcoded = [ 'diarrhea', 'fever', 'stomach cramps' ];
+                        var actual = res.body[i].symptoms;
+                        assert(hardcoded.length == actual.length);
+                        assert(hardcoded.every(function(u, i) {
+                            return u === actual[i];
+                        }));
                     }
                 }
                 assert(contains);
@@ -33,19 +38,30 @@ describe('GET /diseases', function(){
     });
 });
 
-describe('GET /diseases', function(){
-    it('200 response - has salmonella', function(done) {
+describe('GET /disease/777d1c109f5eef1d64c418062a918d33 for salmonella', function(){
+    it('200 response and gets salmonella from id', function(done) {
         request('https://thicc-peas-cdc-api-o54gbxra3a-an.a.run.app')
-            .get('/diseases')
+            .get(`/disease/777d1c109f5eef1d64c418062a918d33`)
+            .expect(200)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200)
             .expect(function(res) {
+                assert(res.body.name == 'salmonella');
+                assert(res.body.disease_id == '777d1c109f5eef1d64c418062a918d33');
+                var hardcoded = [ 'diarrhea', 'fever', 'stomach cramps' ];
+                var actual = res.body.symptoms;
+                assert(hardcoded.length == actual.length);
+                assert(hardcoded.every(function(u, i) {
+                    return u === actual[i];
+                }));
                 var contains = false;
-                for (var i = 0; i < res.body.length; i++) {
-                    if (res.body[i].name == 'salmonella') {
-                        id = res.body[i].disease_id;
+                for (var i = 0; i < res.body.reports.length; i++) {
+                    if (res.body.reports[i].article_id == '0e9180c5077aaa569381beba67e56d96') {
                         contains = true;
+                        assert(res.body.reports[i].report_id == 'c30962a8ba75e5c849fb5f8421a988fc');
+                        assert(res.body.reports[i].article_id == '0e9180c5077aaa569381beba67e56d96');
+                        assert(res.body.reports[i].event_date == '2015-10-16T00:00:00.000Z');
+                        assert(res.body.reports[i].location == 'USA');
                     }
                 }
                 assert(contains);
