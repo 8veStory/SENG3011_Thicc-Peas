60babb07e3ac415e24758a99ee16f9209c865b10
Max Owen <z5263663@cse.unsw.edu.au>
2021-03-26 14:56:58 +1100

    init testing

---
+++ PHASE_1/TestScripts/test/test.js
@@ -0,0 +1,34 @@
+// chai stuff
+var chai = require('chai');  
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+var should = chai.should();  // Using Should style
+
+// supertest stuff
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+request(app)
+  .get('/diseases')
+  .expect('Content-Type', 'text/html; charset=utf-8')
+//   .expect('Content-Length', '15')
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
+
+// mocha test stuff
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
