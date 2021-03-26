4f392034076838b0821702a26f6e642f47e65011
Max Owen <z5263663@cse.unsw.edu.au>
2021-03-26 20:51:51 +1100

    add report tests

--- PHASE_1/TestScripts/test/test.js
+++ PHASE_1/TestScripts/test/test.js
@@ -330,97 +330,286 @@ describe('GET /diseases', function(){
     });
 });
 
-// REPORT TESTING
-describe('GET /reports', function(){
-    it('should respond with a json 200 response with URL in request', function(done) {
+describe('GET /reports', function() {
+    var invalidparams = '?invalidquery=invalidquery';
+    var invalidendpoint = '/invalidendpoint';
+    it('gives the correct status code and response length', function(done) {
         request(`${url}`)
             .get('/reports')
-            .set('Accept', 'application/json')
             .expect(200)
+            .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 9);
+                // console.log(res.body);
+            })
             .end(done);
     });
-    it ('can produce a list of all reports published on outbreaks between given parameters in the form YYYY-MM-DD', function(done){
-        request(`${url}`)
-        .get('/reports?start_date=2020-12-01&end_date=2021-03-01')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(function(res) {
-            console.log(res.body);
-            assert(res.length > 0);
-        })
-        .end(done);
-    });
-    it ('will produce an error when invalid dates in the form of YYYY-MM-DD provided', function(done){
-        request(`${url}`)
-        .get('/reports?start_date=2021-12-01&end_date=2020-03-01')
-        .set('Accept', 'application/json')
-        .expect(400)
-        .end(done);
-    });
-    it ('can produce a list of all reports published on outbreaks at a certain location', function(done){
-        request(`${url}`)
-        .get('/reports?location=USA')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(function(res) {
-            assert(res.length > 0);
-        })
-        .end(done);
-    });
-    it ('will produce an empty list if invalid location supplied', function(done){
-        request(`${url}`)
-        .get('/reports?location=wakanda')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(done);
-    });
-    it ('can produce a list of all reports published on outbreaks containing a desired keyword', function(done){
-        request(`${url}`)
-        .get('/reports?key_terms=hedgehog')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(done);
-    });
-    it ('will produce an empty list if given string does not appear in any given documents', function(done){
-        request(`${url}`)
-        .get('/reports?key_terms=here_is_a_string_of_words_that_can_not_be_in_the_report')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(function(res) {
-            assert(res.length == 0);
-        })
-        .end(done);
-    });
-    it ('will produce a 404 error if invalid query presented', function(done){
-        request(`${url}`)
-        .get('/reports?invalid_query=invalid_query')
-        .set('Accept', 'application/json')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(done);
-    });
-});
 
-describe('GET /report/{reportID}', function(){
-    it('should respond with a json 200 response with URL in request', function(done) {
+    it('?start_date=2018-01-01 only got one report and has correct values', function(done) {
         request(`${url}`)
-            .get('/report/1ed7bef50165f63747e64fc1814fb517')
-            .set('Accept', 'application/json')
+            .get('/reports?start_date=2018-01-01')
             .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 1);
+
+                var report = res.body[0];
+                
+                assert(report.report_id == 'cc2d456d8d1349fefc6f1868b5a6ad8e');
+                assert(report.disease.disease_id == 'b4d780dd311fae981c01e339f90afdae');
+                assert(report.article.article_id == '05cc555686be2f81f16abdd139201bd4');
+                assert(report.location == 'Zimbabwe');
+                assert(report.statistics.hospitalisations == null);
+                assert(report.statistics.reported_cases == 93);
+                assert(report.statistics.deaths == 2);
+                assert(report.event_date == '2018-05-18T00:00:00.000Z');
+            })
+            .end(done);
+    });
+
+    it('?end_date=2000-01-01 only got one report and has correct values', function(done) {
+        request(`${url}`)
+            .get('/reports?end_date=2000-01-01')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 1);
+
+                var report = res.body[0];
+                
+                assert(report.report_id == '2cbf376f73106b9610897251d425979e');
+                assert(report.disease.disease_id == 'b4d780dd311fae981c01e339f90afdae');
+                assert(report.article.article_id == 'b730f0885558d9398d32b87b9cdd25a6');
+                assert(report.location == 'USA');
+                assert(report.statistics.hospitalisations == 2);
+                assert(report.statistics.reported_cases == 2);
+                assert(report.statistics.deaths == 0);
+                assert(report.event_date == '1994-12-30T00:00:00.000Z');
+            })
+            .end(done);
+    });
+
+    it('?start_date=1990-01-01&end_date=2000-01-01 only got one report and has correct values', function(done) {
+        request(`${url}`)
+            .get('/reports?start_date=1990-01-01&end_date=2000-01-01')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 1);
+
+                var report = res.body[0];
+                
+                assert(report.report_id == '2cbf376f73106b9610897251d425979e');
+                assert(report.disease.disease_id == 'b4d780dd311fae981c01e339f90afdae');
+                assert(report.article.article_id == 'b730f0885558d9398d32b87b9cdd25a6');
+                assert(report.location == 'USA');
+                assert(report.statistics.hospitalisations == 2);
+                assert(report.statistics.reported_cases == 2);
+                assert(report.statistics.deaths == 0);
+                assert(report.event_date == '1994-12-30T00:00:00.000Z');
+            })
+            .end(done);
+    });
+
+    it('?start_date=1980-01-01&end_date=1990-01-01 got no reports', function(done) {
+        request(`${url}`)
+            .get('/reports?start_date=1980-01-01&end_date=1990-01-01')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 0);
+            })
+            .end(done);
+    });
+
+    it('?location=Tazmania got one report and has correct values', function(done) {
+        request(`${url}`)
+            .get('/reports?location=Tazmania')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 1);
+
+                var report = res.body[0];
+                
+                assert(report.report_id == 'f8248f4695a4112e938c120fd22dfa63');
+                assert(report.disease.disease_id == 'b4d780dd311fae981c01e339f90afdae');
+                assert(report.article.article_id == '0ba48160bd6034c38b9d5cea8aa94d0a');
+                assert(report.location == 'Tazmania');
+                assert(report.statistics.hospitalisations == null);
+                assert(report.statistics.reported_cases == 23258);
+                assert(report.statistics.deaths == 81);
+                assert(report.event_date == '2016-11-26T00:00:00.000Z');
+            })
+            .end(done);
+    });
+
+    it('?location=unknown got no reports', function(done) {
+        request(`${url}`)
+            .get('/reports?location=unknown')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 0);
+            })
+            .end(done);
+    });
+
+    it('?key_terms=epidemiologically got one report and has correct values', function(done) {
+        request(`${url}`)
+            .get('/reports?key_terms=epidemiologically')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 1);
+
+                var report = res.body[0];
+                
+                assert(report.report_id == '5d61d0a41744a0e18014a9e508bb5e73');
+                assert(report.disease.disease_id == 'b4d780dd311fae981c01e339f90afdae');
+                assert(report.article.article_id == 'c65a4366e9ab94b4b52c2a402072bc98');
+                assert(report.location == 'Kenya');
+                assert(report.statistics.hospitalisations == null);
+                assert(report.statistics.reported_cases == 11033);
+                assert(report.statistics.deaths == 178);
+                assert(report.event_date == '2015-01-06T00:00:00.000Z');
+            })
+            .end(done);
+    });
+
+    it('?key_terms=asdf got no reports', function(done) {
+        request(`${url}`)
+            .get('/reports?location=unknown')
+            .expect(200)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(function(res) {
+                assert(res.body.length == 0);
+            })
+            .end(done);
+    });
+
+    it('check request doesn\'t alter behaviour with extra invalid parameters', function(done) {
+        request(`${url}`)
+            .get(`/reports${invalidparams}`)
+            .expect(200)
+            .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .end(done);
     });
-    it('should respond with a json 404 response if invalid ID', function(done) {
+    it('check request returns 404 status code when given further endpoint', function(done) {
         request(`${url}`)
-            .get('/report/invalid_id')
+            .get(`/reports${invalidendpoint}`)
             .expect(404)
             .expect('Content-Type', "text/html; charset=utf-8")
             .end(done);
     });
 });
+
+// REPORT TESTING
+// describe.only('GET /reports', function(){
+//     it('should respond with a json 200 response with URL in request', function(done) {
+//         request(`${url}`)
+//             .get('/reports')
+//             .set('Accept', 'application/json')
+//             .expect(200)
+//             .expect('Content-Type', /json/)
+//             .expect(function(res) {
+//                 console.log(res.body);
+//                 console.log(res.body.length);
+//                 // assert(res.length > 0);
+//             })
+//             .end(done);
+//     });
+//     it ('can produce a list of all reports published on outbreaks between given parameters in the form YYYY-MM-DD', function(done){
+//         request(`${url}`)
+//         .get('/reports?start_date=2020-12-01&end_date=2021-03-01')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .expect(function(res) {
+//             console.log(res.body);
+//             assert(res.length > 0);
+//         })
+//         .end(done);
+//     });
+//     it ('will produce an error when invalid dates in the form of YYYY-MM-DD provided', function(done){
+//         request(`${url}`)
+//         .get('/reports?start_date=2021-12-01&end_date=2020-03-01')
+//         .set('Accept', 'application/json')
+//         .expect(400)
+//         .end(done);
+//     });
+//     it ('can produce a list of all reports published on outbreaks at a certain location', function(done){
+//         request(`${url}`)
+//         .get('/reports?location=USA')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .expect(function(res) {
+//             assert(res.length > 0);
+//         })
+//         .end(done);
+//     });
+//     it ('will produce an empty list if invalid location supplied', function(done){
+//         request(`${url}`)
+//         .get('/reports?location=wakanda')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .end(done);
+//     });
+//     it ('can produce a list of all reports published on outbreaks containing a desired keyword', function(done){
+//         request(`${url}`)
+//         .get('/reports?key_terms=hedgehog')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .end(done);
+//     });
+//     it ('will produce an empty list if given string does not appear in any given documents', function(done){
+//         request(`${url}`)
+//         .get('/reports?key_terms=here_is_a_string_of_words_that_can_not_be_in_the_report')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .expect(function(res) {
+//             assert(res.length == 0);
+//         })
+//         .end(done);
+//     });
+//     it ('will produce a 404 error if invalid query presented', function(done){
+//         request(`${url}`)
+//         .get('/reports?invalid_query=invalid_query')
+//         .set('Accept', 'application/json')
+//         .expect(200)
+//         .expect('Content-Type', /json/)
+//         .end(done);
+//     });
+// });
+
+// describe('GET /report/{reportID}', function(){
+//     it('should respond with a json 200 response with URL in request', function(done) {
+//         request(`${url}`)
+//             .get('/report/1ed7bef50165f63747e64fc1814fb517')
+//             .set('Accept', 'application/json')
+//             .expect(200)
+//             .expect('Content-Type', /json/)
+//             .end(done);
+//     });
+//     it('should respond with a json 404 response if invalid ID', function(done) {
+//         request(`${url}`)
+//             .get('/report/invalid_id')
+//             .expect(404)
+//             .expect('Content-Type', "text/html; charset=utf-8")
+//             .end(done);
+//     });
+// });
