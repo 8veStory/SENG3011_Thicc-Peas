e844a0f97d8e233a6ce32fd4f2907f2078a691dc
lirichard950316 <47760771+lirichard950316@users.noreply.github.com>
2021-04-20 18:28:37 +1000

    Update NavBar.jsx

--- PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/components/NavBar/NavBar.jsx
+++ PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/components/NavBar/NavBar.jsx
@@ -3,17 +3,30 @@ import { Link } from 'react-router-dom';
 
 import './NavBar.css';
 
-export default function NavBar() {
-  return (
-    <ul className='App-header' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-      <Link className="top_text" to={{pathname: "/register", state: {fromLogIn: true, cameFromLink: true }}}>Log In</Link>
-      <Link className="left_text" to={{pathname: "/register", state: {fromLogIn: false, cameFromLink: true }}}>Sign Up</Link>
+export default function NavBar(props) {
+  if(props.log_status === true){
+    return (
+      <ul className='App-header' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        
+        <a href="/" variant="logout" className="right_text" onClick={() => {
+            console.log(this);
+            props.set_login_status(false);
+          }}>
+          {/* Get rid of log out if user is not signed in. Will probably have
+          to implement some pretty pog authentication in React somehow. */}
+          Log out
+        </a>
+      </ul>
+    );
+  }else{
+    return (
+      <ul className='App-header' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+        <Link className="top_text" to={{pathname: "/register", state: {fromLogIn: true, cameFromLink: true }}}>Log In</Link>
+        <Link className="left_text" to={{pathname: "/register", state: {fromLogIn: false, cameFromLink: true }}}>Sign Up</Link>
 
-      <a href="/" variant="logout" className="right_text" onClick={() => console.log(this)}>
-        {/* Get rid of log out if user is not signed in. Will probably have
-        to implement some pretty pog authentication in React somehow. */}
-        Log out
-      </a>
-    </ul>
-  );
+        
+      </ul>
+    );
+  }
+  
 }
