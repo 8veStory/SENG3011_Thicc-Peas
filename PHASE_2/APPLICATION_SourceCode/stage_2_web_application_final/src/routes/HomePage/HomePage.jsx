2c019275a06f2662e0a68ba013c0740294071c83
Max Owen <z5263663@cse.unsw.edu.au>
2021-04-21 23:02:31 +1000

    add basic stub of home page

--- PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/routes/HomePage/HomePage.jsx
+++ PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/routes/HomePage/HomePage.jsx
@@ -1,31 +1,47 @@
 import mainLogo from '../../images/mainLogo.png';
+import clinicLogo from '../../images/clinicLogo.png';
+import individualLogo from '../../images/individualLogo.png';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import "./HomePage.css";
 
 export default function HomePage() {
-    return (
-        <body>
-            {/* <p>I am a placeholder</p> */}
-            <h1>VaccTracc</h1>
-            <div class="image">
-                <img src={mainLogo} alt="logo"/>
-            </div>
-            <div className="linkButtonWrapper">
-                <Link className="indvButton-text" to={{ pathname: "/" }}>
-                    <div className="linkButton indv">
-                        Individual
-                    </div>
-                </Link>
-                
-                <Link  className="clinicButton-text" to={{ pathname: "/" }}>
-                    <div className="linkButton clinic">
-                        Clinic/Hospital
-                    </div>
-                </Link>
-            </div>
-            
-        </body>
-    );
+  return (
+    <body>
+      {/* <h1>VaccTracc</h1>
+      <div class="image">
+        <img src={mainLogo} alt="logo" />
+      </div>
+      <div className="linkButtonWrapper">
+        <Link className="indvButton-text" to={{ pathname: "/" }}>
+          <div className="linkButton indv">
+            Individual
+          </div>
+        </Link>
+
+        <Link className="clinicButton-text" to={{ pathname: "/" }}>
+          <div className="linkButton clinic">
+            Clinic/Hospital
+          </div>
+        </Link>
+      </div> */}
+
+      <div className="header">
+        <h1>
+          VaccTracc
+        </h1>
+      </div>
+      <div className="choice">
+        <div className="individual-box">
+          <img src={individualLogo}/>
+          <h2>Individual</h2>
+        </div>
+        <div className="clinic-box">
+          <img src={clinicLogo}/>
+          <h2>Clinic</h2>
+        </div>
+      </div>
+    </body>
+  );
 }
