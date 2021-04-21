b434c5f059fe7fd5892dd79ba7462a4329ca9e2e
Max Owen <z5263663@cse.unsw.edu.au>
2021-04-22 03:17:22 +1000

    add left slider tab screen

--- PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/routes/HomePage/LeftSlider.jsx
+++ PHASE_2/APPLICATION_SourceCode/stage_2_web_application_final/src/routes/HomePage/LeftSlider.jsx
@@ -1,6 +1,59 @@
 import React from 'react';
 
 import './LeftSlider.css';
+import forwardArrow from './../../images/arrow-forward.svg';
+
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
 
 export default function LeftSlider(props) {
   let sliderClasses = 'left-slider';
@@ -8,7 +61,49 @@ export default function LeftSlider(props) {
     sliderClasses = 'left-slider open';
   }
 
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <nav className={sliderClasses} onClick={props.hide}></nav>
+    // <nav className={sliderClasses} onClick={props.hide}></nav>
+
+
+
+    <nav className={sliderClasses}>
+      <div className="layout">
+        <div className="individual-pages">
+          <div className={classes.root} style={{height: '100%', backgroundColor: "#F1F7FF"}}>
+            <Tabs
+              orientation="vertical"
+              // variant="scrollable"
+              value={value}
+              onChange={handleChange}
+              aria-label="Vertical tabs example"
+              className={classes.tabs}
+            >
+              <Tab label="Vaccine Finder" {...a11yProps(0)} />
+              <Tab label="Symptom Checker" {...a11yProps(1)} />
+              <Tab label="Local Outbreaks" {...a11yProps(2)} />
+            </Tabs>
+            <TabPanel value={value} index={0}>
+              Vaccine Finder
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+              Symptom Checker
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+              Local Outbreaks
+            </TabPanel>
+          </div>
+        </div>
+        <div className="forward-arrow" onClick={props.hide}>
+          <img src={forwardArrow}/>
+        </div>
+      </div>
+    </nav>
   );
 }
